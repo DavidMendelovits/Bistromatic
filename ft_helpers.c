675c150e910e786f021bb3ccfa7bdf7a7dc63c61
David Mendelovits <david.mendelovits@gmail.com>
2018-07-12 23:03:23 -0700

    upload for mac

--- ft_helpers.c
+++ ft_helpers.c
@@ -81,7 +81,8 @@ int		ft_strlen(const char *str)
 {
 	int		i;
 
-	while (str[i] != '\0')
+	i = 0;
+    while (str[i] != '\0')
 		i++;
 	return (i);
 }
--- parse.c
+++ parse.c
@@ -6,15 +6,16 @@
 /*   By: dmendelo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/12 20:05:27 by dmendelo          #+#    #+#             */
-/*   Updated: 2018/07/12 22:02:10 by dmendelo         ###   ########.fr       */
+/*   Updated: 2018/07/12 20:17:21 by dmendelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bistromatic.h"
+#include <string.h>
 
 int     begin_parse(char *base, int expr_size)
 {
-//	printf("hello\n");
+    printf(")0");
     read_expression(expr_size);
     return (1);
 }
@@ -26,13 +27,16 @@ char    *read_expression(int expr_size)
     char	*_buf;
 
     ret = read(0, buf, expr_size);
-	buf[ret] = '\0';
+    buf[expr_size] = '\0';
+    printf("ret: %i\n", ret);
     if (ret == -1)
     {
         printf("error, no file read");
         return (NULL);
     }
-	_buf = ft_strdup(buf);
+    printf("I hate htis");
+	_buf = strdup(buf);
+    printf("see");
     printf("buffer: %s\n", buf);
     return (_buf);
 }  
